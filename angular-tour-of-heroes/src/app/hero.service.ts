6863147b7771aac7fd750d54f9bf3f7794c9b8cb
Lucas <lucasmoyano90@hotmail.com>
2021-02-15 19:11:32 -0300

    5.In-app Navigation

--- angular-tour-of-heroes/src/app/hero.service.ts
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -7,8 +7,8 @@ import { HEROES } from './mock-heroes';
 import { MessageService } from './message.service';
 
 @Injectable({
-  providedIn: 'root'
-})
+  providedIn: 'root' })
+  
 export class HeroService {
 
   constructor(private messageService: MessageService) { }
@@ -17,6 +17,11 @@ export class HeroService {
     const heroes = of(HEROES);
     this.messageService.add('HeroService: fetched heroes');
     return heroes;
+  }
+  getHero(id: number): Observable<Hero> {
+    // TODO: send the message _after_ fetching the hero
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
   }
 
 }
